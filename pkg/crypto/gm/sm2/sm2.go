92aa4f02e0e0f2fdca2cb4d7546aad76d8c7d9e9
jianlu <jianlu8023@gmail.com>
2024-09-24 21:31:09 +0800

    调整

--- pkg/crypto/gm/sm2/sm2.go
+++ pkg/crypto/gm/sm2/sm2.go
@@ -7,15 +7,50 @@ import (
 	"github.com/tjfoc/gmsm/x509"
 )
 
-func Encrypt(pubKey string, plainText string) ([]byte, error) {
-	pem, err := x509.ReadPublicKeyFromPem([]byte(pubKey))
-	encrypt, err := sm2.Encrypt(pem, []byte(plainText), rand.Reader, sm2.C1C3C2)
-
-	return encrypt, err
+func Encrypt(pubKey string, plaintext string, c1c2c3 bool) ([]byte, error) {
+	return EncryptBytes([]byte(pubKey), []byte(plaintext), c1c2c3)
 }
 
-func Decrypt(privKey string, cipherText []byte) ([]byte, error) {
-	pem, err := x509.ReadPrivateKeyFromPem([]byte(privKey), nil)
-	decrypt, err := sm2.Decrypt(pem, cipherText, sm2.C1C3C2)
-	return decrypt, err
+func EncryptBytes(pubKey []byte, plaintext []byte, c1c2c3 bool) ([]byte, error) {
+	var (
+		encrypt []byte
+		err     error
+	)
+	publicKey, err := x509.ReadPublicKeyFromPem(pubKey)
+	if err != nil {
+		return nil, err
+	}
+	if c1c2c3 {
+		encrypt, err = sm2.Encrypt(publicKey, plaintext, rand.Reader, sm2.C1C2C3)
+	} else {
+		encrypt, err = sm2.Encrypt(publicKey, plaintext, rand.Reader, sm2.C1C3C2)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return encrypt, nil
+}
+
+func Decrypt(privKey, pwd, ciphertext string, c1c2c3 bool) ([]byte, error) {
+	return DecryptBytes([]byte(privKey), []byte(pwd), []byte(ciphertext), c1c2c3)
+}
+
+func DecryptBytes(privKey, pwd, ciphertext []byte, c1c2c3 bool) ([]byte, error) {
+	var (
+		decrypt []byte
+		err     error
+	)
+	privteKey, err := x509.ReadPrivateKeyFromPem(privKey, pwd)
+	if err != nil {
+		return nil, err
+	}
+	if c1c2c3 {
+		decrypt, err = sm2.Decrypt(privteKey, ciphertext, sm2.C1C2C3)
+	} else {
+		decrypt, err = sm2.Decrypt(privteKey, ciphertext, sm2.C1C2C3)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return decrypt, nil
 }
--- pkg/crypto/gm/sm2/sm2_test.go
+++ pkg/crypto/gm/sm2/sm2_test.go
@@ -1,13 +1,70 @@
 package sm2
 
 import (
+	"crypto/rand"
+	"encoding/pem"
 	"testing"
+
+	"github.com/tjfoc/gmsm/sm2"
+	"github.com/tjfoc/gmsm/x509"
 )
 
-func TestCrypto(t *testing.T) {
+func TestEnCrypt(t *testing.T) {
+
+	privateKey, err := sm2.GenerateKey(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	publicKey := &privateKey.PublicKey
+	sm2PublicKey, err := x509.MarshalSm2PublicKey(publicKey)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	pubKeyBlock := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: sm2PublicKey,
+	}
+
+	memory := pem.EncodeToMemory(pubKeyBlock)
+
+	encrypt, err := Encrypt(string(memory), "hello world", true)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	t.Log(encrypt)
+	t.Log(string(encrypt))
 
 }
 
 func TestDecrypt(t *testing.T) {
+	privateKey, err := sm2.GenerateKey(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	sm2PrivateKey, err := x509.MarshalSm2PrivateKey(privateKey, nil)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	priKeyBlock := &pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: sm2PrivateKey,
+	}
+
+	memory := pem.EncodeToMemory(priKeyBlock)
+
+	decrypt, err := Decrypt(string(memory), "", string([]byte{4, 55, 91, 100, 0, 5, 34, 216, 147, 75, 240, 110, 77, 245, 214, 27, 107, 74, 182, 111, 114, 91, 39, 51, 100, 213, 157, 161, 117, 101, 134, 63, 197, 218, 8, 10, 95, 172, 3, 12, 169, 37, 202, 41, 46, 66, 161, 155, 250, 144, 115, 143, 255, 102, 5, 212, 223, 80, 10, 46, 137, 21, 19, 23, 35, 43, 88, 175, 219, 85, 143, 140, 46, 98, 173, 171, 225, 123, 137, 202, 87, 132, 0, 148, 118, 202, 204, 113, 217, 103, 244, 135, 85, 43, 218, 170, 127, 124, 201, 168, 103, 129, 193, 132, 126, 105, 34, 176}), true)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	t.Log(string(decrypt))
 
 }
